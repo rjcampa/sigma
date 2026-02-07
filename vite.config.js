155d393beb210aec76377fb145af1ed7ccf2d3d6
Ryan Campa <ryan.campa@ryzewith.com>
2026-02-06 17:13:18 -0700

    fix nivo build compatibility

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // IMPORTANT: Change this to match your GitHub repo name
-  // If your repo is https://github.com/youruser/sigma-plugins
-  // then base should be "/sigma-plugins/"
-  base: "/sigma-plugins/",
+  base: "/sigma/",
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
